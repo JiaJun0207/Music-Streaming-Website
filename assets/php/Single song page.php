6f995d9ef223062a281987778c14c42b45d8bdea
Melvin Siaw Jian Cheng <melvinsiaw03@gmail.com>
2024-06-14 05:15:54 +0800

    0.2

--- assets/php/Single song page.php
+++ assets/php/Single song page.php
@@ -41,7 +41,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($songDetails['title']); ?></title>
-    <link rel="stylesheet" href="assets\css\phpSSP.css">
+    <link rel="stylesheet" href="css\phpSSP.css">
 </head>
 <body>
     <div class="song-container">
