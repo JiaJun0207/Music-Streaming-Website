ac8e73c26c41b6764d36c4a64633d5bb8afc0e01
Your Name <chongjiensheng@gmail.com>
2024-06-19 03:02:31 +0800

    edit

--- Login.php
+++ Login.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             
             $_SESSION["user_id"] = $user["id"];
             
-            header("Location: User.php");
+            header("Location: User_Home.php");
             exit;
         }
     }
