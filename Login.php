a0989f5d37e8ca582e15f842b45ce498d8a7da9e
JiaJun <100405347+JiaJun0207@users.noreply.github.com>
2024-06-25 04:24:02 +0800

    add process_login.php

--- Login.php
+++ Login.php
@@ -1,39 +1,3 @@
-<?php
-
-$is_invalid = false;
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
-    $conn = require __DIR__ . "/db_connection.php";
-    
-    $sql = sprintf("SELECT * FROM users
-                    WHERE email = '%s'",
-                   $conn->real_escape_string($_POST["email"]));
-    
-    $result = $conn->query($sql);
-    
-    $user = $result->fetch_assoc();
-    
-    if ($user) {
-        
-        if (password_verify($_POST["password"], $user["password_hash"])) {
-            
-            session_start();
-            
-            session_regenerate_id();
-            
-            $_SESSION["user_id"] = $user["user_id"];
-            
-            header("Location: User_Home.php");
-            exit;
-        }
-    }
-    
-    $is_invalid = true;
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
